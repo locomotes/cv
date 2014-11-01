0ba2dc9d8d5c0f6056e81ed20140329fad8f7091
Jonathan Dickerson <jongdickerson@gmail.com>
2014-11-01 18:22:47 -0400

    fixed problem in header

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,7 +3,7 @@
 // skicknav settings 
 
 $(document).ready(function(){
-  $('header').slicknav({
+  $('#menu-cont').slicknav({
     'label' : 'MENU', // Label for menu button. Use an empty string for no label.
     'duplicate': true, // If true, the mobile menu is a copy of the original.
     'duration': true, // The duration of the sliding animation.
